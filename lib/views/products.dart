7d1ea9e0b43158dc3ce4ac88ae86b01ad77f4506
Outcast <muddypredator010@gmail.com>
2026-04-05 20:12:45 +0300

    reached lecture

--- lib/views/products.dart
+++ lib/views/products.dart
@@ -7,6 +7,7 @@ import 'package:get/get_navigation/get_navigation.dart';
 import 'package:http/http.dart' as http;
 
 var myProducts = [];
+bool loaded = false;
 // Product(productname: " cinco", supplier: "Sony", image: ''),
 // Product(productname: "omni", supplier: "marv", image: ''),
 
@@ -18,50 +19,65 @@ class ProductScreen extends StatefulWidget {
 }
 
 class _ProductScreenState extends State<ProductScreen> {
+  @override
+  void initState() {
+    fetchProduct();
+    super.initState();
+  }
+
   fetchProduct() async {
-    var response = await http.get(
+    final response = await http.get(
       Uri.parse('http://localhost/products/read.php'),
     );
+    print(response.body);
     if (response.statusCode == 200) {
       var serverData = jsonDecode(response.body);
       var productData = serverData["data"];
+
       for (var product in productData) {
         myProducts.add(
-          product(
-            name: product["productname"],
+          Product(
+            productname: product["productname"],
             supplier: product["supplier"],
             image: product["image"],
+            price: product["price"],
           ),
         );
       }
+      setState(() {
+        loaded = true;
+      });
     } else {
-      Get.snackbar("Error", " Server error");
+      throw Exception("Failed to load products");
     }
   }
 
+  @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: 4,
+      itemCount: myProducts.length,
       itemBuilder: (context, index) {
-        return Row(
-          children: [
-            Image.network(
-              "http://localhost/products/productimages/" +
-                  myProducts[index].image,
-              width: 40,
-              height: 50,
-            ),
-            Column(
-              children: [
-                Text(myProducts[index].productname),
-                Text(myProducts[index].supplier),
-                Text(myProducts[index].image),
-              ],
-            ),
-          ],
-        );
+        return loaded
+            ? Row(
+                children: [
+                  Image.network(
+                    "http://localhost/products/productimages/" +
+                        myProducts[index].image,
+                    width: 40,
+                    height: 50,
+                  ),
+                  Column(
+                    children: [
+                      Text(myProducts[index].productname),
+                      Text(myProducts[index].supplier),
+                      Text(myProducts[index].image),
+                      Text(myProducts[index].price),
+                    ],
+                  ),
+                ],
+              )
+            : Center(child: CircularProgressIndicator());
       },
     );
-    ;
   }
 }
